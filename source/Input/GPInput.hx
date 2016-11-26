11588c592f009652fe7d68bd2c41b2630527d67f
Simon Weis <laguna.ws@gmail.com>
2016-11-26 20:09:47 +0100

    fixing a bug with gamepad that has not been found

--- source/Input/GPInput.hx
+++ source/Input/GPInput.hx
@@ -11,26 +11,27 @@ class GPInput extends BasicInput
 {
 
 	private var _gamepad : FlxGamepad = null;
-	
+	private var _id : Int = 0;
 	public function new(ID : Int = 0) 
 	{
 		_gamepad = FlxG.gamepads.getByID(ID);
+		_id = ID;
         name = "gp " +  Std.string(ID);
 	}
 	
 	public override function update(elapsed : Float ) : Void
 	{
 		super.update(elapsed);
+		_gamepad = FlxG.gamepads.getByID(_id);
 		if (_gamepad == null)
 			return;
-		
 		xVal = _gamepad.getXAxis(FlxGamepadInputID.LEFT_ANALOG_STICK);
 		yVal = _gamepad.getYAxis(FlxGamepadInputID.LEFT_ANALOG_STICK);
 		
 		xShootVal = _gamepad.getXAxis(FlxGamepadInputID.RIGHT_ANALOG_STICK);
 		yShootVal = _gamepad.getYAxis(FlxGamepadInputID.RIGHT_ANALOG_STICK);
 		
-		if (xVal != 0 || yVal != 0)
+		if (Math.abs(xVal) > 0.1 || Math.abs(yVal) > 0.1)
 		{
 			anyPressed = true;
 		}
