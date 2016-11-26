68312d1f4b600c9e4574852ad06a942ed7b294b0
Jonas U <helloiamjonas@users.noreply.github.com>
2016-11-26 17:48:42 +0100

    menü

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -9,7 +9,12 @@ import flixel.math.FlxMath;
 
 class MenuState extends FlxState
 {
-	private var _text : FlxText;
+	private var _gameTitle : FlxText;
+    private var _gameSubtitle : FlxText;
+    // the int denotes the player number, the bool is true if the player pressed 'A'
+    private var _playerTexts : Map<Int, Bool> = new Map<Int, Bool>();
+  	private var t : Float = 0;
+    
 	//
 	//private var arr : Array<Int> = new Array<Int>();
 	//private var map : Map<Int, String> = new Map<Int,String>();
@@ -19,22 +24,44 @@ class MenuState extends FlxState
 	override public function create():Void
 	{
 		super.create();
+        for (player in 0 ... 4) {
+            _playerTexts.set(player, false);
+        }
+        
+        for (player in 0 ... 4) {
+            trace('Is the player connected?: $_playerTexts[player] ');
+        }
+
 		FlxG.mouse.visible = false;
-		_startText = new FlxText( 100, 100, 0, "pushover, press Space to start", 16);
-        // contains the "player<x>: press A to join" messages
-        _playerText = new Array<String>;
-		add(_startText);
-        add(_playerText)	
+        
+        // tasty spaghetti incoming!
+		_gameTitle = new FlxText(0, GP.fontSize(3), GP.ScreenWidth, "pushover\n", GP.fontSize(3));
+        _gameSubtitle = new FlxText(0, GP.fontSize(3)*2.3, GP.ScreenWidth, "a really nice game", GP.fontSize(6));
+
+	    _gameTitle.alignment = "center";	
+        _gameSubtitle.alignment = "center";
+
+        var format : FlxTextFormat= new FlxTextFormat(0xD81B60);
+        _gameTitle = _gameTitle.addFormat(format, -1, -1);
+
+         add(_gameTitle);
+         add(_gameSubtitle);
+         
+         // add the players
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
 
+       t += elapsed*8;
+       _gameSubtitle.size = Std.int(GP.fontSize(6) - 4 * Math.sin(t));
+		//_text.update(elapsed);
 		if (FlxG.keys.pressed.SPACE)
 		{
 			FlxG.switchState(new PlayState());
 		}
+        
 	}
 	
 	override public function draw():Void
