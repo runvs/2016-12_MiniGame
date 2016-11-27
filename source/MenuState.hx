3a632d6ce2136400a26828a505b84cb8ad67b6f8
Simon Weis <laguna.ws@gmail.com>
2016-11-27 14:37:20 +0100

    cleaning up menu

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -6,73 +6,47 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import flixel.util.FlxAxes;
 
 class MenuState extends FlxState
 {
 	private var _gameTitle : FlxText;
     private var _gameSubtitle : FlxText;
-    // the int denotes the player number, the bool is true if the player pressed 'A'
-    private var _playerTexts : Map<Int, Bool> = new Map<Int, Bool>();
-  	private var t : Float = 0;
-    
-	//
-	//private var arr : Array<Int> = new Array<Int>();
-	//private var map : Map<Int, String> = new Map<Int,String>();
-	//
-	//
+    // _playerTexts contains the text "player <i>: press any key to join" -> if player presses any key, change this text
+ 
+	
 	
 	override public function create():Void
 	{
 		super.create();
-        for (player in 0 ... 4) {
-            _playerTexts.set(player, false);
-        }
-        
-        for (player in 0 ... 4) {
-            trace('Is the player connected?: $_playerTexts[player] ');
-        }
-
 		FlxG.mouse.visible = false;
         
+     
         // tasty spaghetti incoming!
-		_gameTitle = new FlxText(0, GP.fontSize(3), GP.ScreenWidth, "pushover\n", GP.fontSize(3));
-        _gameSubtitle = new FlxText(0, GP.fontSize(3)*2.3, GP.ScreenWidth, "a really nice game", GP.fontSize(6));
-
+		_gameTitle = new FlxText(0, GP.fontSize(2), GP.ScreenWidth, "pushover\n", GP.fontSize(2));
+        _gameSubtitle = new FlxText(0, GP.fontSize(2)*2.3, GP.ScreenWidth, "Press space to start", GP.fontSize(6));
 	    _gameTitle.alignment = "center";	
         _gameSubtitle.alignment = "center";
 
         var format : FlxTextFormat= new FlxTextFormat(0xD81B60);
-        _gameTitle = _gameTitle.addFormat(format, -1, -1);
 
+        var subtitleFormat : FlxTextFormat = new FlxTextFormat(0x00bcd4);
+        _gameTitle.font = "assets/data/MECHAG.TTF";
+        _gameTitle = _gameTitle.addFormat(format, -1, -1);
+        _gameSubtitle = _gameSubtitle.addFormat(subtitleFormat, 6, 11 );
+        _gameSubtitle.font = "assets/data/MECHAG.TTF";
          add(_gameTitle);
          add(_gameSubtitle);
-         
-         // add the players
-		GP.PCols = new PlayerColors();
-		//_startText = new FlxText( 100, 100, 0, "pushover, press Space to start", 16);
-        // contains the "player<x>: press A to join" messages
-        //_playerText = new Array<String>();
-		//add(_startText);
-        //add(_playerText);	
+
 	}
 
 	override public function update(elapsed:Float):Void
 	{
-		super.update(elapsed);
-
-       t += elapsed*8;
-       _gameSubtitle.size = Std.int(GP.fontSize(6) - 4 * Math.sin(t));
-		//_text.update(elapsed);
-		if (FlxG.keys.pressed.SPACE)
-		{
-			FlxG.switchState(new PlayState());
-		}
-        
+	   super.update(elapsed);
 	}
 	
 	override public function draw():Void
 	{
 		super.draw();
-		//_text.draw();
 	}
 }
