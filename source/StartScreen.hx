9df9e67d9a6a3ef3a8181501a8875fd82bbc86de
Jonas U <helloiamjonas@users.noreply.github.com>
2016-12-04 23:51:45 +0100

    fixed misalignment of text on startscreen

--- source/StartScreen.hx
+++ source/StartScreen.hx
@@ -11,39 +11,39 @@ import flixel.util.FlxColor;
 
 class StartScreen extends FlxState
 {
-	private var _gameTitle : FlxText;
+    private var _gameTitle : FlxText;
     private var _gameSubtitle : FlxText;
     // _playerTexts contains the text "player <i>: press any key to join" -> if player presses any key, change this text
     private var _playerTexts : Array<FlxText> = new Array<FlxText>();
-  	private var t : Float = 0;
+    private var t : Float = 0;
     private var input : Array<BasicInput>; 
-	
-	private var _timerText : FlxText; 
-	private var _timer : Float = 120;
-	
+
+    private var _timerText : FlxText; 
+    private var _timer : Float = 120;
+
     private var alreadyJoined : Array<Bool> = new Array<Bool>();
 
     // for keyboards without numpad: keeps track of wether the user choose the shittyinputmode by pressing ENTER
     private var shittyInput : Array<BasicInput>;
     private var _shittyInputMode : Bool = false;
     private var _inputModeIndication : FlxText; 
-	private var _infoText : FlxText;
-	
-	public function getNumberOfPlayersJoined() : Int
-	{
-		var numberOfJoinedPlayers : Int = 0;
-		for (b in alreadyJoined)
-		{
-			if (b) numberOfJoinedPlayers += 1;
-		}
-		
-		return numberOfJoinedPlayers;
-	}
-	
-	override public function create():Void
-	{
-		super.create();
-        
+    private var _infoText : FlxText;
+
+    public function getNumberOfPlayersJoined() : Int
+    {
+        var numberOfJoinedPlayers : Int = 0;
+        for (b in alreadyJoined)
+        {
+            if (b) numberOfJoinedPlayers += 1;
+        }
+
+        return numberOfJoinedPlayers;
+    }
+
+    override public function create():Void
+    {
+        super.create();
+
         // add the FlxText objects to _playerTexts
         for (player in 0 ... 4) {
             _playerTexts.push(new FlxText(0, 300 + player*30 , GP.ScreenWidth, 'P${player + 1}: Press any key to join', GP.fontSize(7)));
@@ -51,8 +51,8 @@ class StartScreen extends FlxState
         }
 
         //trace('$_playerTexts[0]');
-		FlxG.mouse.visible = false;
-        
+        FlxG.mouse.visible = false;
+
         input = new Array<BasicInput>();
         // for laptops without numpad: if shittyInputMode is true, change the InputKeyboard instances to InputShittyKeyboard instances
         // (which are stored in the shittyInput array)
@@ -68,13 +68,13 @@ class StartScreen extends FlxState
         }
         // used to check if specific player has already joined
         for (i in 0 ... input.length) {
-           alreadyJoined.push(false);
+            alreadyJoined.push(false);
         }
 
         // tasty spaghetti incoming!
-		_gameTitle = new FlxText(0, GP.fontSize(2), GP.ScreenWidth, "pushover\n", GP.fontSize(2));
+        _gameTitle = new FlxText(0, GP.fontSize(2), GP.ScreenWidth, "pushover\n", GP.fontSize(2));
         _gameSubtitle = new FlxText(0, GP.fontSize(2)*2.3, GP.ScreenWidth, "Press space to start", GP.fontSize(6));
-	    _gameTitle.alignment = "center";	
+        _gameTitle.alignment = "center";	
         _gameSubtitle.alignment = "center";
 
         var format : FlxTextFormat= new FlxTextFormat(0xD81B60);
@@ -84,64 +84,65 @@ class StartScreen extends FlxState
         _gameTitle = _gameTitle.addFormat(format, -1, -1);
         _gameSubtitle = _gameSubtitle.addFormat(subtitleFormat, 6, 11 );
         _gameSubtitle.font = "assets/data/MECHAG.TTF";
-         add(_gameTitle);
-         add(_gameSubtitle);
-         
-         for (player in _playerTexts) {
+        add(_gameTitle);
+        add(_gameSubtitle);
+
+        for (player in _playerTexts) {
             add(player);
             // change
-//            player.font = "assets/data/MECHAG.TTF";
-         }
-         // add the players
-		GP.PCols = new PlayerColors();
+            //            player.font = "assets/data/MECHAG.TTF";
+        }
+        // add the players
+        GP.PCols = new PlayerColors();
 
-		
-		_timerText = new FlxText(0, 500, 0, 
-		"Game Time: " + Std.string(Std.int(_timer)) + " [PgnUp], [PgnDwn]", 
-		16);
-		_timerText.screenCenter(FlxAxes.X);
-		
-		add(_timerText);
-		
-		_infoText = new FlxText(10, 650, 800,
-	"Created for MiniGameJam in November 2016 by\nThomas Wellmann (graphics), Aldo Brießmann (code),\nStefan Wolf (code), Jonas (code) and Simon Weis (code)\nThe font 'jungle life' by dj urban mecha from dafont.com, BGM videogame2 produced by DL Sounds.", 
-		8);
-		_infoText.color = FlxColor.GRAY;
-		//_infoText.font = "assets/data/MECHAG.TTF";
-		add(_infoText);
-        
+
+        _timerText = new FlxText(0, 500, FlxG.width, 
+                "Game Time: " + Std.string(Std.int(_timer)) + " [PgnUp], [PgnDwn]", 
+                16);
+        _timerText.alignment = "center";
+
+        add(_timerText);
+
+        _infoText = new FlxText(10, 650, 800,
+                "Created for MiniGameJam in November 2016 by\nThomas Wellmann (graphics), Aldo Brießmann (code),\nStefan Wolf (code), Jonas (code) and Simon Weis (code)\nThe font 'jungle life' by dj urban mecha from dafont.com, BGM videogame2 produced by DL Sounds.", 
+                8);
+        _infoText.color = FlxColor.GRAY;
+        //_infoText.font = "assets/data/MECHAG.TTF";
+        add(_infoText);
+
         // displays the input mode (shitty or normal)
         _inputModeIndication = new FlxText(10, 10, FlxG.width, "", 7);
         _inputModeIndication.alignment = "left";
         _inputModeIndication.color = FlxColor.GRAY;
-        
-        add(_inputModeIndication);
-    
-     if (FlxG.sound.music == null)  {
-        FlxG.sound.playMusic("assets/music/Videogame2.wav", 1, true);
-     }
-	}
 
-	override public function update(elapsed:Float):Void
-	{
-	   super.update(elapsed);
+        add(_inputModeIndication);
+
+        if (FlxG.sound.music == null)  {
+            FlxG.sound.playMusic("assets/music/Videogame2.wav", 1, true);
+        }
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
         t += elapsed * 8;
-		
-		_timerText.text = "Game Time: " + Std.string(Std.int(_timer)) + (!_shittyInputMode ? " inc: [PgnUp], dec: [PgnDwn]" : " inc: [1],  dec: [2]");
-		
-		if (FlxG.keys.justPressed.PAGEUP || FlxG.keys.justPressed.ONE)
-		{
-			_timer += 5;
-			if (_timer >= 600) _timer = 600;
-		}
-		if (FlxG.keys.justPressed.PAGEDOWN || FlxG.keys.justPressed.TWO)
-		{
-			_timer -= 5;
-			if (_timer <= 30) _timer = 30;
-		}
-		
-		
-       _gameSubtitle.size = Std.int(GP.fontSize(6) - 4 * Math.sin(t));
+
+        _timerText.text = "Game Time: " + Std.string(Std.int(_timer)) + (!_shittyInputMode ? " inc: [PgnUp], dec: [PgnDwn]" : " inc: [1],  dec: [2]");
+    
+
+        if (FlxG.keys.justPressed.PAGEUP || FlxG.keys.justPressed.ONE)
+        {
+            _timer += 5;
+            if (_timer >= 600) _timer = 600;
+        }
+        if (FlxG.keys.justPressed.PAGEDOWN || FlxG.keys.justPressed.TWO)
+        {
+            _timer -= 5;
+            if (_timer <= 30) _timer = 30;
+        }
+
+
+        _gameSubtitle.size = Std.int(GP.fontSize(6) - 4 * Math.sin(t));
 
         // enable shitty input mode
         if (FlxG.keys.justPressed.ENTER) { //&& getNumberOfPlayersJoined() == 0) {
@@ -155,7 +156,7 @@ class StartScreen extends FlxState
                 input[0] = new InputShittyKeyboard1();//shittyInput[0];
             }
             if (alreadyJoined[1]) {
-                 input[1] = new InputShittyKeyboard2();//shittyInput[1];
+                input[1] = new InputShittyKeyboard2();//shittyInput[1];
             }
 
         } else {
@@ -167,39 +168,39 @@ class StartScreen extends FlxState
                 input[1] = new InputKeyboard2();
             }
         }
-     
+
 
         for (i in 0 ... input.length) {
             input[i].update(elapsed);
             if (alreadyJoined[i]) {
-                 _playerTexts[i].text = input[i].name + " joined";
-                    
+                _playerTexts[i].text = input[i].name + " joined";
+
             } else if (input[i].anyPressed) {
                 alreadyJoined[i] = true;
             }
         }
 
 
-		if (FlxG.keys.pressed.SPACE)
-		{
-			if (getNumberOfPlayersJoined() >= 2)
-			{				
-				var p : PlayState = new PlayState();
-				for (i in 0 ... alreadyJoined.length) {
-				   if (alreadyJoined[i]) {
-						p.addPlayer(input[i].name);
-				   } 
-				}
+        if (FlxG.keys.pressed.SPACE)
+        {
+            if (getNumberOfPlayersJoined() >= 2)
+            {				
+                var p : PlayState = new PlayState();
+                for (i in 0 ... alreadyJoined.length) {
+                    if (alreadyJoined[i]) {
+                        p.addPlayer(input[i].name);
+                    } 
+                }
 
-				p.setTimer(_timer);
-				FlxG.switchState(p);
-			}
-		}
-	}
-	
-	override public function draw():Void
-	{
-		super.draw();
-		//_text.draw();
-	}
-}
+                p.setTimer(_timer);
+                FlxG.switchState(p);
+            }
+        }
+        }
+
+        override public function draw():Void
+        {
+            super.draw();
+            //_text.draw();
+        }
+    }
